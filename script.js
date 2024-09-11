b39969a8cfff70ba75869f4f0f49b67d11694956
Malik Muhammed <maliqlanre@gmail.com>
2024-09-11 20:02:38 +0100

    calculator logic implementation and some other fixes

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const themeElem = document.querySelectorAll(".theme");
 const toggleButton = document.querySelector(".toggle-button");
 const toggleButtonContainer = document.querySelector(".toggle-button-container");
 
+
 // Adds an event listener for each theme element
 themeElem.forEach(element => {
     element.addEventListener("click", () => {
@@ -20,10 +21,10 @@ themeElem.forEach(element => {
 let offsetX;
 
 // Handles the dragging of the toggle button
-toggleButton.addEventListener('mousedown', (e) => {
+toggleButton.addEventListener('mousedown', (e)=> {
     // Calculates the offset between the mouse and the button's position
     offsetX = e.clientX - toggleButton.offsetLeft;
-    console.log("e.clientX:",e.clientX, "toggleButton.offsetLeft:",toggleButton.offsetLeft, "offsetX:",offsetX)
+    // console.log("e.clientX:",e.clientX, "toggleButton.offsetLeft:",toggleButton.offsetLeft, "offsetX:",offsetX)
 
     // Adds event listeners for mouse move and mouse up events
     document.addEventListener('mousemove', moveButton);
@@ -89,3 +90,215 @@ function moveToThemePosition(themeNumber) {
     
     toggleButton.style.left = `${newLeft}px`;
 }
+
+
+
+// Selects the number button elements
+const numberButtons = document.querySelectorAll(".number_btn");
+console.log(numberButtons)
+
+// Selects the calc_screen elements
+const calculatorScreen = document.querySelector(".calc_screen");
+
+// Selects the operator buttons
+
+const operatorButtons = document.querySelectorAll(".operator");
+console.log(operatorButtons)
+
+// Selects the delete button
+
+const deleteButton = document.getElementById("del_btn");
+console.log(deleteButton)
+
+//Selects the equal button
+
+const equalButton = document.querySelector(".equal_btn");
+
+// Selects the reset button
+
+const resetButton = document.querySelector(".reset_btn");
+
+
+let firstNumber = null;
+let currentNumber = "";
+let gettingTheNextNumber = false;
+let operator = null;
+let operatorSelected = false;
+
+
+numberButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        console.log(gettingTheNextNumber)
+
+        if (gettingTheNextNumber) {
+            currentNumber = button.textContent;
+            console.log(currentNumber)
+            gettingTheNextNumber = false;
+        }
+
+        else {
+            
+            if (currentNumber.length >= 15) {
+                return;
+            }
+
+            if (button.textContent === "." && currentNumber.includes(".")) {
+                return;
+            }
+
+            currentNumber += button.textContent;
+            console.log(currentNumber,":",  currentNumber.length)
+        
+        }
+
+        calculatorScreen.textContent = updateScreen(currentNumber)
+
+
+    })
+})
+
+
+operatorButtons.forEach(button => {
+
+    button.addEventListener("click", ()=> {
+
+        console.log(operator, gettingTheNextNumber, currentNumber)
+        
+
+        if (operator && gettingTheNextNumber === false) {
+
+            firstNumber = calculate(firstNumber, Number(currentNumber), operator);
+            calculatorScreen.textContent = firstNumber
+            console.log(typeof firstNumber,  firstNumber)
+            
+        }
+
+        else if (operator === null) {
+            console. log(currentNumber)
+            firstNumber = Number(currentNumber)
+            operatorSelected = true;
+
+            console.log(typeof firstNumber,  firstNumber)
+        }
+
+        else if (operator && gettingTheNextNumber) {
+            currentNumber = "";
+        }
+
+    
+        operator = button.textContent;
+        console.log(operator, "f:", firstNumber, "c:",currentNumber)
+        gettingTheNextNumber = true;
+
+
+    })
+})
+
+// Adds an event listener for equal button
+equalButton.addEventListener("click", () => {
+   
+    console.log(gettingTheNextNumber)
+
+    if (operator && firstNumber !== null && currentNumber !=="" ) {
+       
+        const result = calculate(firstNumber, Number(currentNumber), operator);
+        calculatorScreen.textContent = commaSeparator(result);
+        currentNumber = Number(calculatorScreen.textContent.replace(/,/g, ""))
+        gettingTheNextNumber = false;
+        operator = null;
+        firstNumber = null;
+    }   
+    
+   
+})
+
+// Adds an event listener for reset button
+resetButton.addEventListener("click", () => {
+    gettingTheNextNumber = false;
+    currentNumber = "";
+    firstNumber = null;
+    operator = null;
+    calculatorScreen.textContent = currentNumber
+})
+
+
+// Adds an event listener for the delete button
+deleteButton.addEventListener("click", () => {
+   currentNumber = calculatorScreen.textContent.slice(0, -1).replace(/,/g, "");
+   calculatorScreen.textContent = updateScreen(currentNumber)
+
+})
+
+
+function commaSeparator(numberValue) {
+   
+    if (numberValue - Math.floor(numberValue) !== 0) {
+
+        const decimalLength = numberValue.toString().split(".")[1].length;
+        return numberValue.toLocaleString("en-us", {
+            minimumFractionDigits: decimalLength,
+            maximumFractionDigits: 16,
+        });
+        
+    }
+
+    else {
+        return numberValue.toLocaleString("en-us")
+    }
+}   
+
+// Function to perform basic arithmetic operations
+function calculate(firstOperand, secondOperand, operatorSign) {
+    let result;
+
+    // Convert numbers to Decimal.js objects for precise calculations
+    const firstDecimal = new Decimal(firstOperand);
+    const secondDecimal = new Decimal(secondOperand);
+
+    switch (operatorSign) {
+        case "+":
+            result = firstDecimal.plus(secondDecimal);
+            break;
+        
+        case "-":
+            result = firstDecimal.minus(secondDecimal);
+            break;
+        
+        case "x":
+            result = firstDecimal.times(secondDecimal);
+            break;
+        
+        case "/":
+            result = firstDecimal.div(secondDecimal);
+            break;
+    }
+
+   
+    return result.toNumber(); 
+}
+
+
+
+function updateScreen(value) {
+    const [integerPart, decimalPart] = value.split(".");
+
+    console.log(integerPart, decimalPart)
+
+    const formattedIntegerPart = Number(integerPart).toLocaleString("en-US");
+
+    return decimalPart === undefined 
+        ? formattedIntegerPart 
+        : `${formattedIntegerPart}.${decimalPart}`;
+}
+
+
+
+
+
+
+
+
+
+
+
+
